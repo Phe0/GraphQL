f0b031141e0ee1fa85d5af874921d47a8bc5ef5e
phe0 <pheofo@gmail.com>
2019-01-05 18:51:11 -0200

    Finish setting up mongo database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const grapqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
 
+const Event = require('./models/event')
+
 const app = express()
 
 const events = []
@@ -43,18 +45,33 @@ app.use('/graphql', grapqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event._doc._id.toString() }
+                    })
+                })
+                .catch(err => {
+                    throw err
+                })
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event)
+                date: new Date(args.eventInput.date)
+            })
             return event
+                .save()
+                .then(result => {
+                    console.log(result)
+                    return { ...result._doc, _id: result._doc._id.toString() }
+                })
+                .catch(err => {
+                    console.log(err)
+                    throw err
+                })
         }
     },
     graphiql: true
