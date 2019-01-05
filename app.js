8c509cfb1cb1addfce1de31637d5ed4cff729253
phe0 <pheofo@gmail.com>
2019-01-05 19:20:54 -0200

    make passwords secure by hash and not returning it

--- app.js
+++ app.js
@@ -88,8 +88,14 @@ app.use('/graphql', grapqlHttp({
                 })
         },
         createUser: args => {
-            return bcrypt
-                .hash(args.userInput.password, 12)
+            return User.findOne({email: args.userInput.email})
+                .then(user => {
+                    if(user) {
+                        throw new Error('User exists already')
+                    }
+                    return bcrypt
+                    .hash(args.userInput.password, 12)
+                })
                 .then(hashedPassword => {
                     const user = new User({
                         email: args.userInput.email,
